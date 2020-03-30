a24b248116e006c661335fa4664315949f9e8838
Matsuo Kousuke <k-matsuo@mtk-pc.local>
2020-03-30 21:20:46 +0900

    initialize commit

---
+++ src/main/java/com/mathkm/library/libmanagement/LibManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mathkm.library.libmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathkm/library/libmanagement/LibManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathkm.library.libmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
